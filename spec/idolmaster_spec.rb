a9da3e8def9b26847157f3aa623974e2c8a683c3
hikko624 <hikko624@gmail.com>
2020-06-13 19:43:40 +0900

    add spec

--- spec/idolmaster_spec.rb
+++ spec/idolmaster_spec.rb
@@ -1,9 +1,20 @@
+# coding: utf-8
 require "spec_helper"
 
 RSpec.describe Idolmaster do
   describe "#find" do
-    it " cinderella_girls" do
+    it "cinderella_girls has 190 idols" do
       expect(Idolmaster.find(:cinderella_girls).count).to eq 190
+    end
+  end
+
+  describe "define_methods" do
+    it "cinderella_girls has  " do
+      expect(Idolmaster.cinderella_girls.count).to eq (Idolmaster.find(:cinderella_girls).count)
+    end
+
+    it "cinderella_girls" do
+      expect(Idolmaster.cinderella_girls).to eq (Idolmaster.find(:cinderella_girls))
     end
   end
 end
